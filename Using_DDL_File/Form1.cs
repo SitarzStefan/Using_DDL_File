f8b679c524ae4e50f2ea750fbda924695b0bcae7
Stefan <Stefan.sitarz@gmail.com>
2026-03-06 01:36:48 +0100

    ukończone 4/5_4

--- Using_DDL_File/Form1.cs
+++ Using_DDL_File/Form1.cs
@@ -1,30 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Calculation; // Twoja DLL
 
 namespace Using_DDL_File
 {
     public partial class Form1 : Form
     {
+        // obiekt klasy z DLL
+        calculate cal = new calculate();
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        // Dodawanie
+        private void button2_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                int i = cal.Add(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+                textBox3.Text = i.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        // Odejmowanie
+        private void button1_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                int i = cal.Sub(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+                textBox3.Text = i.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
